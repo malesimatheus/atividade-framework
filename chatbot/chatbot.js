d97576d235249de80f33cc032fe225b119c882e9
Matheus Silvestre <malesimatheus@gmail.com>
2020-06-18 05:06:59 -0300

    chatbot finalizado

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,3 +1,4 @@
+function fazerUmPedido() {
 alert('Aqui você pode fazer o seu pedido');
 
 let nome = window.prompt('Digite seu nome');
@@ -10,33 +11,69 @@ if(cep >= 05500000 && cep <= 09020000) {
     alert('Ainda não atendemos sua região, mas estamos trabalhando para isso');
     }
 
-let pratoPrincipal = ['X-Salada', 'X-Egg', 'X-Bacon', 'X-Tudo']
 
-let precoPratoPrincipal = [16, 17, 17, 23]
+let pratoPrincipal = ['X-Salada', 'X-Egg', 'X-Bacon', 'X-Tudo'];
+
+let precoPratoPrincipal = [16, 17, 17, 23];
 
 let escolhaPratoPrincipal = window.prompt('Escolha o Prato Principal:  1)X-Salada  2)X-Egg  3)X-Bacon 4)X-Tudo (Digite apenas o número)');
 
-if (escolhaPratoPrincipal >= 0 && escolhaPratoPrincipal <= 3) {
-    alert('Ok, você escolheu um ' + pratoPrincipal[escolhaPratoPrincipal - 1])
+let itemPratoPrincipal = (escolhaPratoPrincipal - 1);
+
+if (escolhaPratoPrincipal >= 1 && escolhaPratoPrincipal <= 4) {
+    alert('Ok, você escolheu um ' + pratoPrincipal[itemPratoPrincipal]);
 } else {
-    alert('Ok, você não deseja um Prato Principal')
+    alert('Ok, você não deseja um Prato Principal');
 }
 
-let bebidas = ['Coca', 'Fanta', 'Suco de Laranja', 'Água']
 
-letPrecoBebidas = [5, 5, 7, 3]
+let bebidas = ['Coca', 'Fanta', 'Suco de Laranja', 'Água'];
+
+let precoBebidas = [5, 5, 7, 3];
 
 let escolhaBebida = window.prompt('Escolha sua bebida:  1)Coca  2)Fanta  3)Suco de Laranja 4)Água (Digite apenas o número)');
 
-if (escolhaBebida >= 0 && escolhaBebida <= 3) {
-    alert('Ok, você escolheu ' + bebidas[escolhaBebida - 1])
+let itembebida = (escolhaBebida - 1);
+
+if (escolhaBebida >= 1 && escolhaBebida <= 4) {
+    alert('Ok, você escolheu ' + bebidas[itembebida]);
 } else {
-    alert('Ok, você não deseja uma bebida')
+    alert('Ok, você não deseja uma bebida');
 }
 
-let acompanhamento = ['Batata Frita 300g', 'Nugetts 300g', 'Onion Rings 500g']
 
-let precoAcompanhamento = [7, 9, 9]
+let acompanhamento = ['Batata Frita 300g', 'Nugetts 300g', 'Onion Rings 500g'];
 
-let escolhaAcompanhamento = window.prompt('Escolha seu acompanhamento:  1)Batata Frita 300g  2)Nugetts 300g  3)Onion Rings 500g (Digite aoenas o número)')
+let precoAcompanhamento = [7, 9, 9];
 
+let escolhaAcompanhamento = window.prompt('Escolha seu acompanhamento:  1)Batata Frita 300g  2)Nugetts 300g  3)Onion Rings 500g (Digite apenas o número)');
+
+let itemAcompanhamento = (escolhaAcompanhamento - 1);
+
+if (escolhaAcompanhamento >= 1 && escolhaAcompanhamento <= 4) {
+    alert('Ok, você escolheu ' + acompanhamento[itemAcompanhamento]);
+} else {
+    alert('Ok, você não deseja um acompanhamento');
+}
+
+
+let precoTotal = (precoAcompanhamento[itemAcompanhamento] + precoBebidas[itembebida] + precoAcompanhamento[itemAcompanhamento]);
+
+alert('Você pediu 1 ' + pratoPrincipal[itemPratoPrincipal] + ', 1 ' + bebidas[itembebida] + ', e 1 ' + acompanhamento[itemAcompanhamento]);
+
+alert('O valor do seu pedido é de R$ ' + precoTotal);
+
+let formaDePagamento = window.prompt('Como deseja pagar?  1)Dinheiro  2)Cartão de Débito');
+
+let troco = 0;
+
+if(formaDePagamento = 1) {
+    troco = window.prompt('Deseja troco pra qual valor?');
+    console.log('Troco para ' + troco);
+} else {
+    console.log('Não precisa de troco');
+}
+
+alert('Seu pedido foi realizado com sucesso');
+
+}
